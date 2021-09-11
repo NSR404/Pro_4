577d90a1cf794f7aa175131d4a3bc3f26bb6fad8
NSR404 <alwed404@gmail.com>
2021-09-11 23:08:36 +0300

    professional engineering website

---
+++ js/myscript.js
@@ -0,0 +1,58 @@
+
+
+$(function(){
+   
+     $('li').on("mouseenter",function(){
+            $('li').css({
+                backgroundColor: "#fff",
+                 color: "black"
+            })
+     })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    // $(window).scroll(function(){
+    //     var windowscroll = $(this).scrollTop()
+    //     if(windowscroll > 500){
+    //             $(".btn_direction").fadeIn();
+    //     }
+    //     else{
+    //         $(".btn_direction").fadeOut();
+    //     }
+    // })
+    // $(".btn_direction").on('click',function(){
+    //  $("html").animate({
+    //      scrollTop : 0
+    //  },1000);
+    // })
+
+
+    // // nav slider
+    // var windowwidth = $(window).width();
+    //  if (windowwidth > 320 & windowwidth < 767){
+    //     $("nav").css({
+    //         width:"100%",
+    //         marginTop:"50px",
+    //         marginLeft:"-140px"
+    //     }); 
+        
+    //         $(".link_nav").css({
+    //             padding:"6px",
+    //         });     
+    // }
+    
+    
+ });
